7631db610cea05b7118318a919fcbe6e154ec967
Antoine Lamy <antoinelamy@gmail.com>
2013-05-29 15:31:46 -0400

    Add a viewDidLoadBlock to allow table view creation without the need for subclassing. Also add a frame property for cases where the table view controller should not be full screen.

--- DAModularTableView/DAModularTableViewController.h
+++ DAModularTableView/DAModularTableViewController.h
@@ -12,5 +12,10 @@
 @interface DAModularTableViewController : UITableViewController
 
 @property (nonatomic, strong) DAModularTableView *tableView;
+@property (nonatomic, assign) CGRect frame;
+
+@property (nonatomic, copy) void(^viewDidLoadBlock)();
+
+- (id)initWithFrame:(CGRect)aFrame style:(UITableViewStyle)aStyle;
 
 @end
